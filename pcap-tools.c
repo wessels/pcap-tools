edc6054fdb667a31b5eba7822f9bdcd106a46b58
Duane Wessels <dwessels@verisign.com>
2021-11-19 10:41:01 -0800

    Implement open to command in my_pcap_open_offline(). If the first character is '|' then tokenize string to args and pass to execvp().

--- pcap-tools.c
+++ pcap-tools.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 pcap_t *
 my_pcap_open_offline(const char *pcapfile)
@@ -17,7 +18,35 @@ my_pcap_open_offline(const char *pcapfile)
     fifoname[0] = '\0';
     static unsigned int fifocount = 0;
     pcap_t *in;
-    if (0 == strcmp(pcapfile + strlen(pcapfile) - 3, ".gz")) {
+    assert(pcapfile);
+    if (pcapfile[0] == '|') {
+	char **args = 0;
+	unsigned int nargs = 0;
+	unsigned int i = 0;
+	/* count arguments */
+	nargs++;		/* first arg */
+	for (const char *s = pcapfile; *s; s++) {
+	    if (*s == ' ')
+		nargs++;
+	}
+	nargs++;		/* terminating null */
+	args = calloc(nargs, sizeof(char *));
+	args[0] = strtok((char *) pcapfile + 1, " ");
+	for (unsigned int i = 1; i < nargs - 1; i++) {
+	    args[i] = strtok(NULL, " ");
+	    assert(args[i]);
+	}
+	snprintf(fifoname, 256, "/tmp/fifo.%d.%u", getpid(), fifocount++);
+	mkfifo(fifoname, 0600);
+	if (0 == fork()) {
+	    close(1);
+	    open(fifoname, O_WRONLY);
+	    execvp(args[0], args);
+	    perror(args[0]);
+	    abort();
+	}
+	readfile = fifoname;
+    } else if (0 == strcmp(pcapfile + strlen(pcapfile) - 3, ".gz")) {
 	snprintf(fifoname, 256, "/tmp/fifo.%d.%u", getpid(), fifocount++);
 	mkfifo(fifoname, 0600);
 	if (0 == fork()) {
