d0d635103ff03413cbc072cdac0df94a6694d3f5
Duane Wessels <dwessels@verisign.com>
2021-11-19 10:42:14 -0800

    Implement -m (mergecap) option to pcap-join. Extract timestamp from filenames, sort by timestamp, and then pass all files with same timestamp to a mergecap command. This only works if files all start on consistent time boundaries (as in DITL data).

--- pcap-join.c
+++ pcap-join.c
@@ -12,13 +12,24 @@
 #include <dirent.h>
 #include <getopt.h>
 #include <err.h>
+#include <regex.h>
+#include <assert.h>
 
 #include "pcap-tools.h"
 
 pcap_dumper_t *out = NULL;
-unsigned int fifocount = 0;
 const char *filterstr = 0;
+int opt_mergecap = 0;
 const char *progname = 0;
+const char *mergecap_cmd = "|mergecap -w -";
+
+typedef struct
+{
+    unsigned int size;
+    unsigned int cnt;
+    char **files;
+} filelist;
+
 int verbose = 0;
 
 void
@@ -55,10 +66,58 @@ join(const char *pcapfile)
     my_pcap_close_offline(in);
 }
 
+struct
+{
+    int nmatch;
+    const char *pat;
+    const char *fmt;
+    regex_t *re;
+} time_patterns[] = {
+    {7, "([0-9]{4})([0-9]{2})([0-9]{2}).([0-9]{2}):([0-9]{2}):([0-9]{2})", "%Y%m%d.%H:%M:%S", 0},
+    {0, 0, 0, 0}
+};
+
+time_t
+parse_timestamp(const char *s)
+{
+    regmatch_t pmatch[10];
+    struct tm tm;
+    for (unsigned int k = 0; time_patterns[k].pat; k++) {
+	if (0 == time_patterns[k].re) {
+	    time_patterns[k].re = calloc(1, sizeof(*time_patterns[k].re));
+	    if (0 != regcomp(time_patterns[k].re, time_patterns[k].pat, REG_EXTENDED)) {
+		fprintf(stderr, "regcomp '%s' failed\n", time_patterns[k].pat);
+		exit(1);
+	    }
+	}
+	if (0 != regexec(time_patterns[k].re, s, time_patterns[k].nmatch, pmatch, 0)) {
+	    continue;
+	}
+	assert(pmatch[0].rm_so > -1);
+	if (0 == strptime(s + pmatch[0].rm_so, time_patterns[k].fmt, &tm))
+	    continue;
+	return timegm(&tm);
+    }
+    fprintf(stderr, "no time patterns matched '%s'\n", s);
+    return 0;
+}
+
 int
 qsort_strcmp(const void *a, const void *b)
 {
     return strcmp(*((char **) a), *((char **) b));
+}
+
+int
+qsort_strptime(const void *a, const void *b)
+{
+    time_t time_a = parse_timestamp(*((char **) a));
+    time_t time_b = parse_timestamp(*((char **) b));
+    if (time_a < time_b)
+	return -1;
+    if (time_a > time_b)
+	return 1;
+    return -1;
 }
 
 void
@@ -73,15 +132,40 @@ usage(void)
     exit(1);
 }
 
+filelist *
+filelist_init(void)
+{
+    filelist *fl;
+    fl = calloc(1, sizeof(*fl));
+    fl->size = 64;
+    fl->cnt = 0;
+    fl->files = calloc(fl->size, sizeof(char *));
+    return fl;
+}
+
+void
+filelist_add(filelist * fl, const char *file)
+{
+    if (fl->cnt == fl->size) {
+	char **tmp = calloc(fl->size << 1, sizeof(char *));
+	memcpy(tmp, fl->files, fl->size * sizeof(char *));
+	free(fl->files);
+	fl->files = tmp;
+	fl->size <<= 1;
+    }
+    fl->files[fl->cnt++] = strdup(file);
+}
+
 int
 main(int argc, char *argv[])
 {
     int i;
+    filelist *infiles = 0;
     if (strrchr(argv[0], '/'))
 	progname = strdup(1 + strrchr(argv[0], '/'));
     else
 	progname = strdup(argv[0]);
-    while ((i = getopt(argc, argv, "b:hv")) != -1) {
+    while ((i = getopt(argc, argv, "b:hmv")) != -1) {
 	switch (i) {
 	case 'b':
 	    filterstr = strdup(optarg);
@@ -90,6 +174,10 @@ main(int argc, char *argv[])
 	case 'h':
 	default:
 	    usage();
+	    break;
+	case 'm':
+	    opt_mergecap = 1;
+	    break;
 	case 'v':
 	    verbose++;
 	    break;
@@ -98,12 +186,14 @@ main(int argc, char *argv[])
     argc -= optind;
     argv += optind;
 
+    infiles = filelist_init();
+
     if (0 == argc) {
 	char buf[512];
 	/* read file names from stdin */
 	while (NULL != fgets(buf, 512, stdin)) {
 	    strtok(buf, "\r\n");
-	    join(buf);
+	    filelist_add(infiles, buf);
 	}
     } else
 	for (i = 0; i < argc; i++) {
@@ -112,50 +202,66 @@ main(int argc, char *argv[])
 		err(1, "%s", argv[i]);
 	    if (S_ISDIR(sb.st_mode)) {
 		DIR *d;
-		unsigned int filecnt = 0;
-		unsigned int k;
 		struct dirent *e;
-		char **paths;
 		/*
-		 * count files
+		 * read unsorted files
 		 */
 		d = opendir(argv[i]);
 		if (NULL == d)
 		    err(1, "%s", argv[i]);
 		while (NULL != (e = readdir(d))) {
-		    if (*e->d_name == '.')
-			continue;
-		    filecnt++;
-		}
-		closedir(d);
-		/*
-		 * read unsorted files
-		 */
-		paths = calloc(filecnt, sizeof(char *));
-		k = 0;
-		d = opendir(argv[i]);
-		if (NULL == d)
-		    err(1, "%s", argv[i]);
-		while (k < filecnt && NULL != (e = readdir(d))) {
 		    char path[512];
 		    if (*e->d_name == '.')
 			continue;
 		    snprintf(path, sizeof(path), "%s/%s", argv[i], e->d_name);
-		    *(paths + (k++)) = strdup(path);
+		    filelist_add(infiles, path);
 		}
 		closedir(d);
-		if (k < filecnt)
-		    filecnt = k;
-		qsort(paths, filecnt, sizeof(char *), qsort_strcmp);
-		for (k = 0; k < filecnt; k++) {
-		    join(*(paths + k));
-		    free(*(paths + k));
-		}
-		free(paths);
+		qsort(infiles->files, infiles->cnt, sizeof(char *), qsort_strcmp);
+
 	    } else {
-		join(argv[i]);
+		filelist_add(infiles, argv[i]);
 	    }
 	}
+
+    if (opt_mergecap) {
+	size_t bufsize = 0;
+	unsigned int j = 0;
+	unsigned int k = 0;
+	filelist *new = 0;
+	new = filelist_init();
+	qsort(infiles->files, infiles->cnt, sizeof(char *), qsort_strptime);
+	while (j < infiles->cnt) {
+	    char *buf = 0;
+	    bufsize = strlen(mergecap_cmd);
+	    while (k < infiles->cnt && parse_timestamp(infiles->files[j]) == parse_timestamp(infiles->files[k])) {
+		bufsize += 1 + strlen(infiles->files[k]);
+		k++;
+	    }
+	    bufsize += 1;	/* terminating null */
+	    buf = calloc(bufsize, sizeof(char *));
+	    strcat(buf, mergecap_cmd);
+	    for (unsigned int i = j; i < k; i++) {
+		strcat(buf, " ");
+		strcat(buf, infiles->files[i]);
+	    }
+	    assert(strlen(buf) == bufsize - 1);
+	    filelist_add(new, buf);
+	    j = k;
+	    free(buf);
+	    buf = 0;
+	}
+	for (unsigned int i = 0; i < infiles->cnt; i++)
+	    free(infiles->files[i]);
+	free(infiles);
+	infiles = new;
+    }
+
+    for (unsigned int i = 0; i < infiles->cnt; i++) {
+	join(infiles->files[i]);
+	free(infiles->files[i]);
+    }
+    free(infiles);
     if (out)
 	pcap_dump_close(out);
     exit(0);
