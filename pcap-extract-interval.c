70478b408d4dcfdb871d1f6772f9d33a811a9dff
Duane Wessels <dwessels@verisign.com>
2023-01-27 14:59:21 -0800

    pcap-extract-interval: add -x option to exit upon end timestamp

--- pcap-extract-interval.c
+++ pcap-extract-interval.c
@@ -6,6 +6,17 @@
 #include <string.h>
 #include <time.h>
 #include <errno.h>
+#include <getopt.h>
+
+static void
+usage(void)
+{
+    fprintf(stderr, "usage: pcap-extract-interval [-x] begin end\n");
+    fprintf(stderr, "\tbegin\tUnix timestamp\n");
+    fprintf(stderr, "\tend\tUnix timestamp\n");
+    fprintf(stderr, "\t-x\texit at first timestamp after end time\n");
+    exit(1);
+}
 
 int
 main(int argc, char *argv[])
@@ -17,14 +28,28 @@ main(int argc, char *argv[])
     const u_char *data;
     time_t beg;
     time_t end;
+    int ch;
+    int opt_exit = 0;
 
-
-    if (argc < 3) {
-	fprintf(stderr, "usage: pcap-extract-interval begin end\n");
-	exit(1);
+    while ((ch = getopt(argc, argv, "x")) != -1) {
+        switch (ch) {
+        case 'x':
+            opt_exit = 1;
+            break;
+        case '?':
+        case 'h':
+	default:
+            usage();
+            break;
+        }
     }
-    beg = atoi(argv[1]);
-    end = atoi(argv[2]);
+
+
+    if ((argc - optind) < 2) {
+	usage();
+    }
+    beg = atoi(argv[optind]);
+    end = atoi(argv[optind+1]);
 
     in = pcap_open_offline("-", errbuf);
     if (NULL == in) {
@@ -37,8 +62,16 @@ main(int argc, char *argv[])
 	exit(1);
     }
     while ((data = pcap_next(in, &hdr))) {
-	if (hdr.ts.tv_sec >= beg && hdr.ts.tv_sec <= end)
-	    pcap_dump((void *) out, &hdr, data);
+	if (hdr.ts.tv_sec < beg)
+	    continue;
+	if (hdr.ts.tv_sec >= end) {
+	    if (opt_exit) {
+		exit(0);
+	    } else {
+		continue;
+	    }
+	}
+	pcap_dump((void *) out, &hdr, data);
     }
     pcap_close(in);
     pcap_dump_close(out);
